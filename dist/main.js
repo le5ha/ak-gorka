668cbbd014105fbf2325b9f0e2e77ac254c65264
lesha <leshapopovkin@MacBook-Pro-Lesha.local>
2024-01-14 15:25:22 +0400

    ghpages setup

--- dist/main.js
+++ dist/main.js
@@ -13,20 +13,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   Carousel: () => (/* binding */ Carousel)
 /* harmony export */ });
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
-function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 var Carousel = /*#__PURE__*/function () {
-  function Carousel(slidesClass, leftButtonSelector, rightButtonSelector) {
+  function Carousel(trackSelector, leftButtonSelector, rightButtonSelector) {
     var _this = this;
     _classCallCheck(this, Carousel);
-    this.slidePosition = 0;
-    this.slides = document.getElementsByClassName(slidesClass);
+    this.slideIndex = 0;
+    this.track = document.querySelector(trackSelector);
+    this.slides = Array.from(this.track.children);
     this.totalSlides = this.slides.length;
     this.leftButton = document.querySelector(leftButtonSelector);
     this.rightButton = document.querySelector(rightButtonSelector);
@@ -36,36 +34,59 @@ var Carousel = /*#__PURE__*/function () {
     this.rightButton.addEventListener('click', function () {
       return _this.move(1);
     });
+    this.startX = 0;
+    this.track.addEventListener('touchstart', function (e) {
+      return _this.startSwipe(e);
+    });
+    this.track.addEventListener('touchmove', function (e) {
+      return _this.moveSwipe(e);
+    });
+    this.track.addEventListener('touchend', function () {
+      return _this.endSwipe();
+    });
+    this.updateSlides();
   }
   _createClass(Carousel, [{
-    key: "updateSlidePosition",
-    value: function updateSlidePosition() {
-      var _iterator = _createForOfIteratorHelper(this.slides),
-        _step;
-      try {
-        for (_iterator.s(); !(_step = _iterator.n()).done;) {
-          var slide = _step.value;
-          slide.classList.remove('carousel__slide_type_current');
-          slide.classList.add('carousel__slide_type_hidden');
-        }
-      } catch (err) {
-        _iterator.e(err);
-      } finally {
-        _iterator.f();
+    key: "startSwipe",
+    value: function startSwipe(e) {
+      this.startX = e.touches[0].clientX;
+    }
+  }, {
+    key: "moveSwipe",
+    value: function moveSwipe(e) {
+      var currentX = e.touches[0].clientX;
+      var diffX = this.startX - currentX;
+      if (diffX > 0) {
+        // Swiped left
+        this.move(1);
+      } else if (diffX < 0) {
+        // Swiped right
+        this.move(-1);
       }
-      this.slides[this.slidePosition].classList.add('carousel__slide_type_current');
+    }
+  }, {
+    key: "endSwipe",
+    value: function endSwipe() {
+      this.startX = 0;
     }
   }, {
     key: "move",
     value: function move(n) {
-      this.slidePosition += n;
-      if (this.slidePosition < 0) {
-        this.slidePosition = this.totalSlides - 1;
-      } else if (this.slidePosition === this.totalSlides) {
-        this.slidePosition = 0;
+      this.slideIndex += n;
+      if (this.slideIndex < 0) {
+        this.slideIndex = this.totalSlides - 1;
+      } else if (this.slideIndex >= this.totalSlides) {
+        this.slideIndex = 0;
       }
-      console.log('it works');
-      this.updateSlidePosition();
+      this.updateSlides();
+    }
+  }, {
+    key: "updateSlides",
+    value: function updateSlides() {
+      var _this2 = this;
+      this.slides.forEach(function (slide, index) {
+        slide.style.display = index === _this2.slideIndex ? 'block' : 'none';
+      });
     }
   }]);
   return Carousel;
@@ -152,7 +173,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_Carousel_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/Carousel.js */ "./src/components/Carousel.js");
 
 
-var carousel = new _components_Carousel_js__WEBPACK_IMPORTED_MODULE_1__.Carousel('carousel__slide', '.carousel__button_type_left', '.carousel__button_type_right');
+var carousel = new _components_Carousel_js__WEBPACK_IMPORTED_MODULE_1__.Carousel('.carousel__track', '.carousel__button_type_left', '.carousel__button_type_right');
 })();
 
 /******/ })()
