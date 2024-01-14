0839084eb9872b0e9a484dda652f75bb83d9b910
lesha <leshapopovkin@yandex.ru>
2024-01-15 00:51:31 +0400

    feat: video and contacts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import './index.css';
 import { Carousel } from '../components/Carousel.js';
 
-const carousel = new Carousel('.carousel__track', '.carousel__button_type_left', '.carousel__button_type_right');
+const carouselAbout = new Carousel('.carousel__track-about', '.carousel__button_type_left', '.carousel__button_type_right');
+const carouselplans = new Carousel('.carousel__track-plans', '.carousel__button_type_left', '.carousel__button_type_right');
