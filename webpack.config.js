c028cf8e7df46f5e6927aba514455d555379ee2b
lesha <leshapopovkin@yandex.ru>
2024-01-16 11:37:34 +0400

    try

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
-        publicPath: '/repo/'
+        publicPath: '/ak-gorka/'
     },
     mode: 'development',
     plugins: [
