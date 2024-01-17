59fd9ce8375658ac268b5a50eafe822e4001a17a
lesha <leshapopovkin@yandex.ru>
2024-01-17 18:45:30 +0400

    Updates

--- main.js
+++ main.js
@@ -91,6 +91,52 @@ var Carousel = /*#__PURE__*/function () {
 
 /***/ }),
 
+/***/ "./src/components/ResponsiveLink.js":
+/*!******************************************!*\
+  !*** ./src/components/ResponsiveLink.js ***!
+  \******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   ResponsiveLink: () => (/* binding */ ResponsiveLink)
+/* harmony export */ });
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+var ResponsiveLink = /*#__PURE__*/function () {
+  function ResponsiveLink(selector, mobileHref, desktopHref, breakpoint) {
+    _classCallCheck(this, ResponsiveLink);
+    this.link = document.querySelectorAll(selector);
+    this.mobileHref = mobileHref;
+    this.desktopHref = desktopHref;
+    this.breakpoint = breakpoint;
+    this.updateHref = this.updateHref.bind(this);
+    window.addEventListener('resize', this.updateHref);
+    this.updateHref();
+  }
+  _createClass(ResponsiveLink, [{
+    key: "updateHref",
+    value: function updateHref() {
+      var _this = this;
+      var width = window.innerWidth;
+      this.link.forEach(function (link) {
+        if (width < _this.breakpoint) {
+          link.setAttribute('href', _this.mobileHref);
+        } else {
+          link.setAttribute('href', _this.desktopHref);
+        }
+      });
+    }
+  }]);
+  return ResponsiveLink;
+}();
+
+/***/ }),
+
 /***/ "./src/pages/index.css":
 /*!*****************************!*\
   !*** ./src/pages/index.css ***!
@@ -168,10 +214,13 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.css */ "./src/pages/index.css");
 /* harmony import */ var _components_Carousel_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/Carousel.js */ "./src/components/Carousel.js");
+/* harmony import */ var _components_ResponsiveLink_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/ResponsiveLink.js */ "./src/components/ResponsiveLink.js");
+
 
 
 var carouselAbout = new _components_Carousel_js__WEBPACK_IMPORTED_MODULE_1__.Carousel('.carousel__track-about', '.carousel__button-about.carousel__button_type_left', '.carousel__button-about.carousel__button_type_right');
 var carouselPlans = new _components_Carousel_js__WEBPACK_IMPORTED_MODULE_1__.Carousel('.carousel__track-plans', '.carousel__button-plans.carousel__button_type_left', '.carousel__button-plans.carousel__button_type_right');
+new _components_ResponsiveLink_js__WEBPACK_IMPORTED_MODULE_2__.ResponsiveLink('.link__call', 'tel:+79184087741', '#contacts', 600);
 })();
 
 /******/ })()
